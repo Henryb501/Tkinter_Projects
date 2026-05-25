b4352881af9eaa33fc7a9c95a54e0b62184a38ff
Henryb501 <henrybailey501@gmail.com>
2026-05-25 12:37:53 -0500

    Inti start

---
+++ main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import sqlite3
+
+# Create the database and set up the cursor for you
+conn = sqlite3.connect('Tutorial.db')
+cu = conn.cursor()
+
+app = Tk()
+app.title("My Address Book")
+app.geometry("500x500")
+
+# all DATABASE functions go at the top of your program
+def create_table():
+    cu.execute("CREATE TABLE IF NOT EXISTS people(name TEXT,age INTEGER,height TRAL)")
+
+def data_entry(name, age, height):
+    cu.execute('INSERT INTO people VALUES(?,?,?)', (name, age, height))
+    conn.commit()
+    # add treeview row insert here
+
+def get_data():
+    cu.execute('SELECT rowid, * FROM people')
+    data = cu.fetchall()
+    for d in data:
+        print(d)
+        # HERE need to reload the treeview here
+
+def update():
+    cu.execute('UPDATE people SET age = 30 WHERE rowid = 1')
+    conn.commit()
+    get_data()
+
+def delete():
+    cu.execute('DELETE FROM people WHERE rowid = 1')
+    conn.commit()
+    get_data()
+
+# all other functions go here
+
+
+# Add your program here
+
+
+app.mainloop()
+conn.close()
